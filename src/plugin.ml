e96031fe40c470cf813f2ebf8797437790164ed4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-05-29 16:37:47 -0500

    refactor(plugin): never deal with `//topleve//` buffers

--- src/plugin.ml
+++ src/plugin.ml
@@ -1,25 +1,8 @@
 
 module L = Reason_lexer
 
-let rec skip_phrase lexbuf =
-  try
-    match L.token lexbuf with
-    | Reason_parser.SEMI | Reason_parser.SEMISEMI | Reason_parser.EOF -> ()
-    | _ -> skip_phrase lexbuf
-  with
-  | L.Error (L.Unterminated_comment _, _)
-  | L.Error (L.Unterminated_string, _)
-  | L.Error (L.Unterminated_string_in_comment _, _)
-  | L.Error (L.Illegal_character _, _) -> skip_phrase lexbuf
-
-let maybe_skip_phrase lexbuf =
-  if Parsing.is_current_lookahead Reason_parser.SEMI
-  || Parsing.is_current_lookahead Reason_parser.SEMISEMI
-  || Parsing.is_current_lookahead Reason_parser.EOF
-  then ()
-  else skip_phrase lexbuf
-
 let wrap_internal lex_fun parsing_fun lexbuf =
+  Location.init lexbuf "toplevel"; (* remove smartass printing *)
   try
     Docstrings.init ();
     let ast = parsing_fun lex_fun lexbuf in
@@ -27,18 +10,8 @@ let wrap_internal lex_fun parsing_fun lexbuf =
     Docstrings.warn_bad_docstrings ();
     ast
   with
-  | Reason_lexer.Error(Reason_lexer.Illegal_character _, _) as err
-    when !Location.input_name = "//toplevel//"->
-    skip_phrase lexbuf;
-    raise err
-  | Syntaxerr.Error _ as err
-    when !Location.input_name = "//toplevel//" ->
-    maybe_skip_phrase lexbuf;
-    raise err
   | Parsing.Parse_error | Syntaxerr.Escape_error ->
     let loc = Location.curr lexbuf in
-    if !Location.input_name = "//toplevel//"
-    then maybe_skip_phrase lexbuf;
     raise(Syntaxerr.Error(Syntaxerr.Other loc))
 
 let wrap ~post parsing_fun lexbuf =
